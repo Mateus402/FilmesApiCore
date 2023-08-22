0ab62d32755b1817b720ee16bce8aef9d0031be8
Alexandre de Moraes <alexandresmoraes@me.com>
2023-08-22 14:14:26 -0300

    up

--- FilmesApi/Program.cs
+++ FilmesApi/Program.cs
@@ -3,19 +3,15 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddEntityFrameworkNpgsql()
-  .AddDbContext<FilmeContext>(opts => opts.UseNpgsql("User ID=postgres;Password=s3nh4;Host=host.docker.internal;Port=5432;Database=curso_alura;"));
+  .AddDbContext<FilmeContext>(opts => opts.UseNpgsql(builder.Configuration.GetConnectionString("FilmeConnection")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
   app.UseSwagger();
